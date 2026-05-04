08c113320aed72c86511f7b1185cff388fad7410
NamIT1 <namn98561@gmail.com>
2026-05-05 01:50:04 +0700

    update css for folder frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './assets/css/index.css';
+import './assets/css/style.globals.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
