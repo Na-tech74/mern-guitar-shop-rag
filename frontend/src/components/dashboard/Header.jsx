21905f9589c359d1bcfbd265923afe6578fc01cb
NamIT1 <namn98561@gmail.com>
2026-05-08 10:20:41 +0700

    remove unused files

---
+++ frontend/src/components/dashboard/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <div className="flex items-center justify-between mb-6">
+            <h1 className="text-2xl font-bold">Dashboard</h1>
+            <div>
+             
+            </div>
+        </div>
+    );
+}
