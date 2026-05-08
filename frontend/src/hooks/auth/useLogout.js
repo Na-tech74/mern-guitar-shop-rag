df189bbdc17d045ac2f6b5c0ad3b2af9a0d1ca91
NamIT1 <namn98561@gmail.com>
2026-05-08 12:41:36 +0700

    modified:   frontend/src/hooks/auth/useLogout.js

--- frontend/src/hooks/auth/useLogout.js
+++ frontend/src/hooks/auth/useLogout.js
@@ -3,7 +3,7 @@ export default function useLogout() {
     const navigate = useNavigate();
     // lấy thông tin người dùng từ localStorage
     const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-    const handleLogout = () => {
+    const handleLogout = () =>  {
         // xóa thông tin người dùng khỏi localStorage
         localStorage.removeItem("userInfo");
         // nếu có refresh token, xóa nó khỏi localStorage
@@ -12,8 +12,8 @@ export default function useLogout() {
         // chuyển hướng về trang đăng nhập
         navigate("/");
     }
-    return ({
-        userInfo,
-        handleLogout
-    });
-};
+        return ({
+            userInfo,
+            handleLogout
+        });
+    };
